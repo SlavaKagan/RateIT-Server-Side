a96fdde121d40dccd9c6d90718286ad951b208c9
YossiSaadi <YossiSaadi@172.40.1.26>
2018-10-29 12:26:47 +0200

    Finally !

---
+++ src/main/java/main/RateITApp.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import main.logic.TrailerPane;
+
+//@SpringBootApplication
+public class RateITApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		@SuppressWarnings("resource")
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter search term: ");
+		TrailerPane tpane = new TrailerPane(s.nextLine());
+		stage.setScene(new Scene(tpane));
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
