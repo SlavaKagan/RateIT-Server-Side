85391a26bdef88181215ae719eb9b1839b5ba2c8
YossiSaadi <YossiSaadi@YogaYS>
2018-10-29 22:02:05 +0200

    UserTo - half way, any ideas?

---
+++ src/main/java/playground/logic/UserTo.java
@@ -0,0 +1,12 @@
+package playground.logic;
+
+public class UserTo {
+	private String email;
+	private String playground;
+	private String userName;
+	private String avatar;
+	private String role;
+	private long points;
+	
+	
+}
