65c669a651ec427106e76be94b840424e5c9d416
Yarden-PC <Yarden-PC@Yarden-PC>
2018-10-30 12:11:50 +0200

    Created the NewUserForm class

---
+++ src/main/java/playground/logic/NewUserForm.java
@@ -0,0 +1,44 @@
+package playground.logic;
+
+public class NewUserForm {
+	private String email;
+	private String username;
+	private String avatar;
+	private String role;
+	
+	public NewUserForm() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
