3cdddcc687437fef9f1eec573b5aa38549c29e4d
slava <slava@DESKTOP-NCKVUJS.Home>
2018-11-17 20:44:10 +0200

    Add PUT method for element

--- src/test/java/playground/layout/WebUITestElements.java
+++ src/test/java/playground/layout/WebUITestElements.java
@@ -28,11 +28,10 @@ public class WebUITestElements {
 	
 	@Autowired
 	private UserService userservice;
-	
 	private RestTemplate restTemplate;
+	private String oldId;
 	private String url;
 	private NewUserForm form;
-	
 	public static final String EMAIL = "rubykozel@gmail.com";
 	
 	@LocalServerPort
@@ -195,6 +194,7 @@ public class WebUITestElements {
 		//Then the response is 500 with 
 	}
 	
+	
 	public void testCreatingAnElementWithEmptyJSON() throws Exception {
 		
 		/*
@@ -261,5 +261,43 @@ public class WebUITestElements {
 				actualElement.getAttributes(),
 				Constants.PLAYGROUND,
 				EMAIL);
+	}
+	
+	@Test
+	public void testChangeTheIdOfTheElement() throws Exception{
+	/*
+	 	Given the server is up
+		And theres an element with playground: "2019A.Kagan", email: "rubykozel@gmail.com", playground: "2019A.Kagan", id: "2061451755",
+	*/
+		userservice.createUser(form);
+		ElementTO newElement= new ElementTO();
+		elementservice.createElement(newElement, Constants.PLAYGROUND, EMAIL);
+		
+		//When I PUT "/playground/elements/2019A.Kagan/rubykozel@gmail.com/2019A.Kagan/2061451755" with '{"playground": "2019A.Kagan","id": "765","location": {"x": 3.6830377111762047,"y": 8.3868617407449,"name": "Messaging Board","creationDate": "2018-11-11T19:19:18.786+0000","expirationDate": "2018-11-12T19:19:18.786+0000","type": "Messaging Board","attributes": {"creatorsName": "Manager","isActive": "True","isAMovie": "False","movieName": "Venom 2018"},"creatorPlayground": "2019A.Kagan","creatorEmail": "rubykozel@gmail.com"}'
+		
+		oldId=newElement.getId();
+		newElement.setId("765");
+		
+		this.restTemplate.put(url + "/{userPlayground}/{email}/{playground}/{id}", newElement, Constants.PLAYGROUND,EMAIL,Constants.PLAYGROUND,oldId);
+		//Then the reponse is "200 OK"
+	}
+	
+	@Test //(expected = Exception.class)
+	public void testTryingToChangeSomeAttributeWithNull() throws Exception{
+	/*
+		Given the server is up
+		And there is an element with playground: "2019A.Kagan", email: "rubykozel@gmail.com", playground: "2019A.Kagan", id: "567",
+	*/
+		userservice.createUser(form);
+		ElementTO newElement= new ElementTO("Messaging Board","Messaging Board",Constants.PLAYGROUND,EMAIL);
+		elementservice.createElement(newElement, Constants.PLAYGROUND, form.getEmail());
+		
+		//When I PUT "/playground/elements/2019A.Kagan/rubykozel@gmail.com/2019A.Kagan/567" with '{id:null}'
+		
+		oldId=newElement.getId();
+		newElement.setId(null);
+		
+		this.restTemplate.put(url + "/{userPlayground}/{email}/{playground}/{id}", newElement, Constants.PLAYGROUND,EMAIL,Constants.PLAYGROUND,oldId);
+		//Then the reponse is 500 with message
 	}
 }
