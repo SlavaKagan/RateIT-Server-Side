2ef8e3bab955c90313cbb0759a38e2f539c9c351
slava <slava@DESKTOP-NCKVUJS.Home>
2018-12-20 22:45:36 +0200

    Fix User Validation

--- src/main/java/playground/aop/annotations/UserValidatorAspect.java
+++ src/main/java/playground/aop/annotations/UserValidatorAspect.java
@@ -6,26 +6,26 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import playground.logic.UserService;
+import playground.dal.UserDao;
 
 @Component
 @Aspect
 public class UserValidatorAspect {
 	
-	private UserService userService;
+	private UserDao user;
 	
 	@Value("${delim:@@}")
 	private String delim;
 	
 	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
+	public void setUserService(UserDao user) {
+		this.user = user;
 	}
 	
 	@Around("@annotation(playground.aop.annotations.ValidateUser) && args(userPlayground,email,..)")
 	public Object validateUser(ProceedingJoinPoint jp, String userPlayground, String email) throws Throwable {
 		try {
-			if(userService.getUser(userPlayground + delim + email).equals(null))
+			if(user.findById(userPlayground + delim + email).equals(null))
 				return jp.proceed();
 			else
 				throw new Exception("User doesn't exist in database!");
