23d981cef4753a07b45b4be1acb3bb0d7dcc5f44
kozel <kozel@172.40.0.87>
2018-12-21 03:07:59 +0200

    added @Component to PostMessagePlugin

--- src/main/java/playground/plugins/PostMessagePlugin.java
+++ src/main/java/playground/plugins/PostMessagePlugin.java
@@ -2,9 +2,13 @@ package playground.plugins;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import playground.logic.ActivityEntity;
 
+@Component
 public class PostMessagePlugin implements Plugin {
 	
 	private ObjectMapper jackson;
